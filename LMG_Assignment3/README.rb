820470f4fdb7a6afd64baba00980fc46e90e4fba
Lucía Muñoz Gil <84689114+lucis-e@users.noreply.github.com>
2023-12-06 19:19:16 +0100

    Update README.rb

--- LMG_Assignment3/README.rb
+++ LMG_Assignment3/README.rb
@@ -1,1 +1,7 @@
+# CODE FOR ASSIGNMENT #3-, GFF feature files and visualization
 
+```
+This code was created in collaboration with my colleague Miguel La Iglesia Mirones
+Lucía Muñoz Gil, MSc in Computational Biology, Bioinformatics Programming Challenges course
+November 2023
+```
