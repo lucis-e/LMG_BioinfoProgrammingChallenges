44685a3de2a3695d7b0a1cf3b2c5ec79dfed073f
Lucía Muñoz Gil <84689114+lucis-e@users.noreply.github.com>
2023-12-06 19:23:07 +0100

    Delete LMG_Assignment3/README.rb

--- LMG_Assignment3/README.rb
+++
@@ -1,7 +0,0 @@
-# CODE FOR ASSIGNMENT #2: intensive integration using web APIs
-
-```
-This code was created in collaboration with my colleague Miguel La Iglesia Mirones
-Lucía Muñoz Gil, MSc in Computational Biology, Bioinformatics Programming Challenges course
-November 2023
-```
